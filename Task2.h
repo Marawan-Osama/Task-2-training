3d3ba695f5f484b7b6fdc9e11323dc8dc6e3e82f
Marawan-Osama <68340597+Marawan-Osama@users.noreply.github.com>
2020-07-19 23:45:58 +0200

    Update Task2.h

--- Task2.h
+++ Task2.h
@@ -3,12 +3,12 @@
 
 typedef unsigned char uint8_t;
 
-void push(uint8_t u8_data);
+void push(uint8_t u8_data); /* Pushes variable to the top of the stack*/
 
-uint8_t pull();
+uint8_t pull(); /*pulls back the top variable in the stack and decreases the index in the stack array by 1*/
 
-void printStack();
+void printStack(); /*Prints all the elements in the stack array*/
 
-uint8_t* balancedParantheses(uint8_t* expression);
+uint8_t* balancedParantheses(uint8_t* expression); /*Checks to see if the string inserted has equal number and shapes of parantheses on both sides*/
 
-int PairUp(uint8_t bracket1 , uint8_t bracket2);
+int PairUp(uint8_t bracket1 , uint8_t bracket2); /*Checks to see if the shape of the brackets are correct in both shape and respective position on either side*/
