56c439aab6fae3b1daba767eb3eb322ab06f5f49
Marawan-Osama <68340597+Marawan-Osama@users.noreply.github.com>
2020-07-19 23:45:26 +0200

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -6,12 +6,13 @@ uint8_t array[100];
 int location = -1;
 
 int main(){
-uint8_t* a = balancedParantheses("(({}))");
+uint8_t* a = balancedParantheses("(({}))");   /* This is to call the function*/
+
 printf("%s",a);
 }
 
 void push(uint8_t u8_data){
-    location++;
+    location++; 
     array[location] = u8_data;
     
 }
@@ -21,9 +22,9 @@ uint8_t pull(){
 }
 
 void printStack(){
-    for (int i = location; i > -1; i--)
+    for (int i = location; i > -1; i--) /*loops all elements*/
     {
-        printf("%c",array[i]);
+        printf("%c",array[i]); /*prints elements one by one*/
     }
     
 }
@@ -35,27 +36,32 @@ int PairUp(uint8_t bracket1 , uint8_t bracket2){
         return 1;
     else
         return 0;
+        /*checks for the validity of conditions*/
 }       
 
 uint8_t* balancedParantheses(uint8_t* expression){
     for (int i = 0; i < strlen(expression); i++)
     {
-        if(expression[i] == '(' || expression[i] == '{'){
+        if(expression[i] == '(' || expression[i] == '{') /*Checks if the expressions are the open brackets needed to push*/
+        {
             push(expression[i]);
         }
-        if(expression[i] == ')' || expression[i] == '}'){
+        if(expression[i] == ')' || expression[i] == '}')
+        {
         
-                char temp = pull();
-                if(!PairUp(temp,expression[i])){
+                char temp = pull(); /*creates new temp variable to store last pushed character*/
+                if(!PairUp(temp,expression[i])) /*if the two variables do not pair up, it will return not balanced*/
+                {
                     return "Not balanced";
                 }
         
         }
     }
-    if(location == -1){
+    if(location == -1) /*if the index returns to -1 then that is the end of the pushed array so the number of brackets end*/
+    {
         return "Balanced";
     }
     else{
-        return "Not balanced";
+        return "Not balanced"; /* there are still brackets in the push array so it is not balanced*/
     }
 }
